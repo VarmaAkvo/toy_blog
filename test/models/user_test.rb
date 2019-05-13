1a88866a29c08251f2e0d3767f48e3e5fe17fa85
VarmaAkvo <choice_1274475898@yahoo.co.jp>
2019-05-13 13:40:34 +0800

    给Users表添加了name列

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+    assert @user.valid_password?('123456')
+  end
+
+  test 'name should be present' do
+    @user.name = ''
+    assert_not @user.valid?
+  end
+
+  test 'name should be uniq and not case sensitive' do
+    @user.name = users(:two).name
+    assert_not @user.valid?
+    @user.name.upcase!
+    assert_not @user.valid?
+  end
 end
