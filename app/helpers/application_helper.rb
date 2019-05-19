6209ffc0aae35447c3117c3125c09cbced440bec
VarmaAkvo <choice_1274475898@yahoo.co.jp>
2019-05-19 10:57:52 +0800

    添加了默认头像

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
             end.join.html_safe
     list = content_tag(:div, items, class: "alert alert-danger mt-2")
   end
+
+  def avatar_of(user, size)
+    #默认头像
+    style = "height: #{size}px;width: #{size}px;"
+    text_style = "font-size: #{size/2}px;line_height: #{size/2}px;"
+    name = content_tag(:span, user.name.first.capitalize, class: "text-center text-white", style: text_style)
+    content_tag(:div, name, class: "rounded-circle bg-info d-flex justify-content-center align-items-center", style: style)
+  end
 end
