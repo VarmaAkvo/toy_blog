e5ef575783c7f6e248d63f4ed7e05733ebceaadc
VarmaAkvo <choice_1274475898@yahoo.co.jp>
2019-05-27 10:56:17 +0800

    修改了首页视图

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
+    # 选出最受欢迎的9个用户的{id: follower_count}
+    @ranking_users_hash = Relation.group(:followed_id).order(count_follower_id: :desc)
+                                .limit(9).count(:follower_id)
+    @ranking_users = User.includes(:tags, :avatar_attachment).find(@ranking_users_hash.keys)
+    # 如果用户已登录，则选出所关注的博客中最新的9条
+    if user_signed_in?
+      @recent_following_articles = Article.includes(:tags, user: :avatar_attachment).where(user_id: current_user.following_ids)
+                                          .order(created_at: :desc).limit(9)
+    end
+    # 最新文章
+    @recent_articles = Article.includes(:tags, user: :avatar_attachment).order(created_at: :desc).limit(9)
   end
 end
