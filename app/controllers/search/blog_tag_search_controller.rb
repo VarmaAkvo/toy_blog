5d2346b4c4305b390fe034620530b4009539c76d
VarmaAkvo <choice_1274475898@yahoo.co.jp>
2019-06-03 17:37:33 +0800

    修改了一些视图的样式，优化了博客内tag搜索以及用户tag搜索的查询，解决了它们的关联tag不预加载的问题

--- app/controllers/search/blog_tag_search_controller.rb
+++ app/controllers/search/blog_tag_search_controller.rb
@@ -6,8 +6,13 @@ class Search::BlogTagSearchController < ApplicationController
       redirect_to root_path and return
     end
     tag = params[:tag]
-    @articles = @owner.articles.joins(:tags).where(tags: {name: tag})
-                      .order(created_at: :desc).page(params[:page]).per_page(10)
+    # 用joins不会预加载关联
+    article_ids = @owner.articles.joins(:tags)
+                      .where(tags: {name: tag})
+                      .order(created_at: :desc).page(params[:page]).per_page(10).ids
+    @articles = Article.with_rich_text_content_and_embeds.includes(:tags)
+                       .where(id: article_ids)
+                       .order(created_at: :desc).page(params[:page]).per_page(10)
     @uats = UserArticlesTagsStatistic.includes(:tag).where(user_id: @owner.id)
   end
 end
--- app/controllers/search/user_tags_controller.rb
+++ app/controllers/search/user_tags_controller.rb
@@ -2,10 +2,13 @@ class Search::UserTagsController < ApplicationController
   def index
     tag = params[:tag]
     # 返回符合条件的用户并按关注数排列
-    @users = User.includes(:avatar_attachment).joins(:tags, :passive_relations).where(tags: {name: tag})
+    @user_ids = User.joins(:tags, :passive_relations).where(tags: {name: tag})
                       .group(:id).order(Arel.sql('COUNT(relations.id) DESC'))
-                      .page(params[:page]).per_page(9)
-    @users_follower_count = Relation.where(followed_id: @users.ids)
+                      .page(params[:page]).per_page(9).ids
+    @users = User.includes(:avatar_attachment, :tags)
+                 .where(id: @user_ids).page(params[:page]).per_page(9)
+    @users_hash = @users.map {|user| [user.id, user]}.to_h
+    @users_follower_count = Relation.where(followed_id: @users_ids)
                                     .group(:followed_id).count(:follower_id)
   end
 end
